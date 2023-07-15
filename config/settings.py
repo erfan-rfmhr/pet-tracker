615945312e2306d39ab8c6782f3d08b0c3940e69
erfan <erfan.arefmehr@gmail.com>
2023-07-15 13:16:04 +0330

    initialize core user and pet owner apps and models. set auth_user_model to core user. register core user and petowner to admin

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'petowner',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.CoreUser'
