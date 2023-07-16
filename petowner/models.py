db1dd702a007256866295c0a9b1dbd3f4e435206
erfan <erfan.arefmehr@gmail.com>
2023-07-16 11:59:27 +0330

    feat: add phone,address and image to petowner model

--- petowner/models.py
+++ petowner/models.py
@@ -6,3 +6,6 @@ from core.models import CoreUser
 # Create your models here.
 class PetOwner(models.Model):
     user = models.OneToOneField(CoreUser, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=12)
+    address = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', default='images/default.png')
