c59281bc5f716c04f4b2253ab8d05bc0c9325739
erfan <erfan.arefmehr@gmail.com>
2023-07-18 16:12:16 +0330

    add password field to petowner form

--- petowner/forms.py
+++ petowner/forms.py
@@ -8,10 +8,11 @@ class PetOwnerCreationForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
     username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput)
 
     class Meta:
         model = PetOwner
-        fields = ['first_name', 'last_name', 'username', 'phone', 'address', 'image']
+        fields = ['first_name', 'last_name', 'phone', 'address', 'username', 'password', 'image']
 
     def save(self, commit=True):
         instance = super(PetOwnerCreationForm, self).save(commit=False)
@@ -19,6 +20,7 @@ class PetOwnerCreationForm(forms.ModelForm):
             username=self.cleaned_data['username'],
             first_name=self.cleaned_data['first_name'],
             last_name=self.cleaned_data['last_name'],
+            password=self.cleaned_data['password'],
         )
         instance.user = user
         if commit:
