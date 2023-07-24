9c3bba5cce4bc9a958a945aaa59acb807575621c
erfan <erfan.arefmehr@gmail.com>
2023-07-24 16:53:51 +0330

    create pet model. migrate pet model.

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'core',
+    'pet',
     'petowner',
     'staff',
 ]
--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from petowner.models import PetOwner
+
+
 # Create your models here.
+class PetModel(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    age = models.IntegerField()
+    birthdate = models.DateField()
+    petowner = models.ForeignKey(PetOwner, on_delete=models.CASCADE, related_name='pets')
+
+    def __str__(self):
+        return self.name
