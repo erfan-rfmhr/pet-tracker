984f1a843ca8a6f7706930b312b5066b60f68719
erfan <erfan.arefmehr@gmail.com>
2023-07-24 16:27:09 +0330

    move image field from petowner to core user

--- core/models.py
+++ core/models.py
@@ -5,3 +5,4 @@ from django.db import models
 # Create your models here.
 class CoreUser(AbstractUser):
     is_customer = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='images/', default='images/default.png')
--- petowner/models.py
+++ petowner/models.py
@@ -8,4 +8,3 @@ class PetOwner(models.Model):
     user = models.OneToOneField(CoreUser, on_delete=models.CASCADE, related_name='petowner')
     phone = models.CharField(max_length=12)
     address = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/', default='images/default.png')
