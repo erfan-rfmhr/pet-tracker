3d22623eda3a9f1b4c1e0dd178d88e012c44378f
erfan <erfan.arefmehr@gmail.com>
2023-07-29 10:09:14 +0330

    update: add image and serial number field to pet model

--- pet/models.py
+++ pet/models.py
@@ -10,6 +10,8 @@ class PetModel(models.Model):
     breed = models.CharField(max_length=255)
     age = models.IntegerField()
     birthdate = models.DateField()
+    image = models.ImageField(upload_to='images/', default='images/pet-logo.png')
+    serial_number = models.CharField(max_length=255)
     petowner = models.ForeignKey(PetOwner, on_delete=models.CASCADE, related_name='pets')
 
     def __str__(self):
