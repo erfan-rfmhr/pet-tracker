236145c67c378414a744801efcaa7a06497f923e
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-08-28 16:11:57 +0330

    change coordinate and temperature view temporary. get a single text and send it back as response

--- pet/views.py
+++ pet/views.py
@@ -2,13 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
-from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .forms import PetModelForm
-from .models import PetModel, PetTemperatureModel
-from .serializers import PetTemperatureSerializer, PetCoordinateSerializer
+from .models import PetModel
 
 
 class PetDeleteAPIView(APIView):
@@ -31,11 +29,11 @@ class PetListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'pets'
 
 
-class PetTemperatureCreateAPIView(generics.CreateAPIView):
-    serializer_class = PetTemperatureSerializer
-    queryset = PetTemperatureModel.objects.all()
+class PetTemperatureCreateAPIView(APIView):
+    def post(self, request):
+        return Response(data=request.data)
 
 
-class PetCoordinateCreateAPIView(generics.CreateAPIView):
-    serializer_class = PetCoordinateSerializer
-    queryset = PetTemperatureModel.objects.all()
+class PetCoordinateCreateAPIView(APIView):
+    def post(self, request):
+        return Response(data=request.data)
