2973e9499c8044c2f83d7044f7df2dc51379cf44
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-09-02 08:07:34 +0330

    write custom plain text parser and renderer

--- pet/views.py
+++ pet/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
+from rest_framework import renderers, status
+from rest_framework.parsers import BaseParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -29,15 +31,36 @@ class PetListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'pets'
 
 
+class PlainTextParser(BaseParser):
+    media_type = 'text/plain'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        return stream.read()
+
+
+class PlainTextRenderer(renderers.BaseRenderer):
+    media_type = 'text/plain'
+    format = 'txt'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return str(renderers.JSONRenderer().render(data, media_type, renderer_context)).encode(self.charset)
+
+
 class PetTemperatureCreateAPIView(APIView):
+    renderer_classes = (PlainTextRenderer,)
+    parser_classes = (PlainTextParser,)
+
     def post(self, request):
-        with open('file.txt', 'a') as f:
+        with open('file.txt', 'wb') as f:
             f.write(request.data)
-        return Response(data=request.data)
+        return Response(data=request.data, status=status.HTTP_201_CREATED)
 
 
 class PetCoordinateCreateAPIView(APIView):
+    parser_classes = (PlainTextParser,)
+    renderer_classes = (PlainTextRenderer,)
+
     def post(self, request):
-        with open('file.txt', 'a') as f:
+        with open('file.txt', 'wb') as f:
             f.write(request.data)
-        return Response(data=request.data)
+        return Response(data=request.data, status=status.HTTP_201_CREATED)
