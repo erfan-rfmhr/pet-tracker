df965c2e8fd7e863eafe4fc99369ad5ef1fe9c99
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-09-02 16:44:15 +0330

    remove age field in pet form. calculate age from birthdate value

--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 from petowner.models import PetOwner
@@ -8,11 +10,15 @@ class PetModel(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
     breed = models.CharField(max_length=255)
-    age = models.IntegerField()
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
     birthdate = models.DateField()
     image = models.ImageField(upload_to='images/', default='images/pet-logo.png')
     serial_number = models.CharField(max_length=255, unique=True)
     petowner = models.ForeignKey(PetOwner, on_delete=models.CASCADE, related_name='pets')
+
+    def save(self, *args, **kwargs):
+        self.age = (date.today() - self.birthdate).days // 365
+        super(PetModel, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
