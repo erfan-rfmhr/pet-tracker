5396584f8c8995deebf2ea9ee1e2fee5d4ed2e14
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-09-09 13:34:12 +0330

    pop application and pass from temperature and coordinate serializers

--- pet/serializers.py
+++ pet/serializers.py
@@ -19,6 +19,11 @@ class PetTemperatureSerializer(serializers.ModelSerializer):
         model = PetTemperatureModel
         fields = ('temperature', 'pet')
 
+    def to_internal_value(self, data):
+        data.pop('application')
+        data.pop('pass')
+        return super().to_internal_value(data)
+
 
 class PetCoordinateSerializer(serializers.ModelSerializer):
     pet = serializers.SlugRelatedField(slug_field='serial_number', queryset=PetModel.objects.all())
@@ -26,3 +31,8 @@ class PetCoordinateSerializer(serializers.ModelSerializer):
     class Meta:
         model = PetCoordinateModel
         fields = ('point', 'pet')
+
+    def to_internal_value(self, data):
+        data.pop('application')
+        data.pop('pass')
+        return super().to_internal_value(data)
