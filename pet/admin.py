a31083ee5939a30835ac946b5f4caab2209f07c9
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-09-11 11:40:16 +0330

    register temperature and coordinate in admin

--- pet/admin.py
+++ pet/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import PetModel
+from .models import PetModel, PetTemperatureModel, PetCoordinateModel
 
 # Register your models here.
 admin.site.register(PetModel)
+admin.site.register(PetTemperatureModel)
+admin.site.register(PetCoordinateModel)
