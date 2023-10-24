4b0c7f55ee671467dd3968f5b14ef4301b009761
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-10-24 15:42:30 +0330

    chore: some changes in model

--- pet/models.py
+++ pet/models.py
@@ -26,7 +26,7 @@ class PetModel(models.Model):
 
 class PetTemperatureModel(models.Model):
     pet = models.ForeignKey(PetModel, on_delete=models.CASCADE, related_name='temperatures')
-    temperature = models.DecimalField(max_digits=40, decimal_places=10)
+    temperature = models.DecimalField(max_digits=15, decimal_places=5)
     date = models.DateField(auto_now_add=True)
     time = models.TimeField(auto_now_add=True)
 
